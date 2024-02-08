5f8d2156ea10484fd943faa840d3641bf82d6468
stully18 <stullyrl@gmail.com>
2024-02-08 14:34:58 -0500

    typing app

--- TypingGame/main.py
+++ TypingGame/main.py
@@ -16,19 +16,20 @@ count = 4
 sampleSentences = sentences()
 startTime = 0
 
-def submit(event):
+def submit():
     global startTime
     global currentSentence
     userInput = userEntry.get(1.0,END)
-    accuracy = calculate_accuracy(userInput, currentSentence)
+    accuracy,sentence_length = calculate_accuracy(userInput, currentSentence)
     typingTime = round(time.time()-startTime,2)
-    timeWpm.config(text=f"Your time was {typingTime} seconds and your accuracy was {accuracy}%")
+    wpm = round((sentence_length/typingTime)*60)
+    timeWpm.config(text=f"Time: {typingTime} seconds\nAccuracy: {accuracy}%\nWPM: {wpm}")
 def calculate_accuracy(user_input, sentence):
     user_words = user_input.strip().split()  # Split user input into words
     sentence_words = sentence.strip().split()  # Split sentence into words
     correct_words = [uw == sw for uw, sw in zip(user_words, sentence_words)]  # Compare each word
-    accuracy = sum(correct_words) / len(sentence_words)  # Calculate accuracy
-    return accuracy
+    accuracy = (sum(correct_words)) / len(sentence_words)  # Calculate accuracy
+    return round(accuracy*100),len(sentence_words)
 
 def start(event):
     global startTime
@@ -49,18 +50,27 @@ def start(event):
 window = Tk()
 window.geometry("720x400")
 window.title('Typing Test')
-
+menubar = Menu(window)
+window.config(menu=menubar)
 window.resizable(False, False)
-window.bind("<Return>", submit)
 window.bind("<space>", start)
 window.config(bg=BG)
+
+fileMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="change color")
+
 practiceSentence = Message(font=('Consolas', 15), width=700,bg=BG,fg=FG,
                            text="Welcome to this typing test! Press spacebar to get started and the enter key when you are finished with the sentence")
 practiceSentence.grid(row=0, column=0)
 
-userEntry = Text(font=('Consolas', 15), width=50, height=3,state="disabled",bg=BG2,fg=FG)
+userEntry = Text(font=('Consolas', 15), width=50, height=3,state="disabled",bg=BG2,fg=FG,wrap="word")
 userEntry.grid(row=1, column=0)
+
 timeWpm = Message(font=('Consolas', 15),width=700,bg=BG,fg=FG)
-timeWpm.grid(row=2,column=0)
+timeWpm.grid(row=3,column=0)
+
+submitBtn = Button(text="submit",command=submit,font=('Consolas', 15))
+submitBtn.grid(row=2,column=0)
+
 
 window.mainloop()
--- TypingGame/sampleSentences.py
+++ TypingGame/sampleSentences.py
@@ -1,3 +1,3 @@
 def sentences():
-    sampleSentences = ["Please take your dog, Cali, out for a walk – he really needs some exercise!","""What a beautiful day it is on the beach, here in beautiful and sunny Hawaii.""","Rex Quinfrey, a renowned scientist, created plans for an invisibility machine.","Do you know why all those chemicals are so hazardous to the environment?"]
+    sampleSentences = ["Please take your dog, Cali, out for a walk, he really needs some exercise!","""What a beautiful day it is on the beach, here in beautiful and sunny Hawaii.""","Rex Quinfrey, a renowned scientist, created plans for an invisibility machine.","Do you know why all those chemicals are so hazardous to the environment?"]
     return sampleSentences
