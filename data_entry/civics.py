15f011ff8ba014e5b7a8a99c26f970b56b688b53
stully18 <stullyrl@gmail.com>
2024-09-16 14:24:28 -0400

    leet code

--- data_entry/civics.py
+++ data_entry/civics.py
@@ -1,5 +1,7 @@
 import re
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright
+from funcCivics import *
+import time
 
 def login(page):
     page.goto("https://www.measuringuplive.com/")
@@ -11,6 +13,13 @@ def login(page):
 
 
 def createQuestion(page):
+    print_reference = input("Enter print reference\n")
+    difficulty_level = input("Enter difficulty level 1-5\n")
+    state_difficulty = input("State difficulty level (E,A,D)\n")
+    RBT = input("Enter RBT (Remember, Understand, Apply, Analyze, Create, Evaluate) \n")
+    BKD = input("Enter Blooms Knowledge Dimension (F,C,P,M)\n")
+    WDK = input("Enter Depth on knowledge level (1,2,3,4)\n")
+
     page.get_by_label("Content linkPress enter or").click()
     page.get_by_label("Question Search linkPress").click()
     page.get_by_title("Create Question").locator("span").click()
@@ -25,30 +34,24 @@ def createQuestion(page):
     div_locator = "body > div.me-container.common-bg > div.container > section > div > div > div.tab-holder.create-question-tab-holder > div > div > div.general-header.panel-collapse.collapse.in > div:nth-child(2) > div.question-id > div.question-id-text"
     div_text = page.locator(div_locator).inner_text()
     print(f'Question ID: {div_text}')
-    # print_reference = input("Enter print reference\n")
-    page.locator("input[type=\"text\"]").fill("testing")
-    page.locator("#difficultyLevels-input").select_option("1")
-    page.locator("#difficultyLevels-input").select_option("5")
-    page.locator("#difficultyLevels-input").select_option("2")
-    page.locator("#difficultyLevels-input").select_option("3")
-    page.locator("#difficultyLevels-input").select_option("4")
-    page.get_by_text("A - Average").click()
-    page.get_by_text("D - Difficult").click()
-    page.get_by_text("E - Easy").click()
-    page.locator("#blooms-input").select_option("2")
-    page.locator("#blooms-input").select_option("1")
-    page.locator("#blooms-input").select_option("4")
-    page.locator("#blooms-input").select_option("6")
-    page.locator("#blooms-input").select_option("3")
-    page.locator("#blooms-input").select_option("5")
-    page.locator("#RevisedBlooms-input").select_option("1")
-    page.locator("#RevisedBlooms-input").select_option("4")
-    page.locator("#RevisedBlooms-input").select_option("3")
-    page.locator("#RevisedBlooms-input").select_option("2")
-    page.locator("#Wook-input").select_option("1")
-    page.locator("#Wook-input").select_option("4")
-    page.locator("#Wook-input").select_option("3")
-    page.locator("#Wook-input").select_option("2")
+    page.locator("input[type=\"text\"]").fill(print_reference)
+    #difficulty levels
+    page.locator("#difficultyLevels-input").select_option(str(DLdict[difficulty_level]))
+    #state difficulty levels
+    if state_difficulty.lower() == "e":
+        state_difficulty = "E - Easy"
+    elif state_difficulty.lower() == "a":
+        state_difficulty = "A - Average"
+    elif state_difficulty.lower() == "d":
+        state_difficulty = "D - Difficult"
+
+    page.get_by_text(state_difficulty).click()
+    #revised blooms taxonomy
+    page.locator("#blooms-input").select_option(RBTdic[RBT])
+    #blooms knowledge dimension
+    page.locator("#RevisedBlooms-input").select_option(BKDdict[BKD.upper()])
+    #Webbs Depth of knowledge
+    page.locator("#Wook-input").select_option(WDKdict[str(WDK)])
     page.locator("#create-question-nav-tab-3").click()
     page.locator(".authoring-template-container").click()
     page.get_by_text("Multiple Choice/Multiple").click()
@@ -69,7 +72,7 @@ def createQuestion(page):
     page.locator("div:nth-child(4) > .row-container > .row-selection-container").click()
     page.locator("#tab-holder").get_by_text("Next").click()
     page.frame_locator("iframe[title=\"Rich Text Editor\\, editor6\"]").locator("body").click()
-    page.frame_locator("iframe[title=\"Rich Text Editor\\, editor6\"]").locator("body").fill("cue a ")
+    page.frame_locator("iframe[title=\"Rich Text Editor\\, editor6\"]").locator("body").fill("cue a")
     page.frame_locator("iframe[title=\"Rich Text Editor\\, editor7\"]").locator("html").click()
     page.frame_locator("iframe[title=\"Rich Text Editor\\, editor7\"]").locator("body").fill("cue b")
     page.frame_locator("iframe[title=\"Rich Text Editor\\, editor8\"]").get_by_text("Enter answer cue").click()
@@ -85,17 +88,20 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-
+    login(page)  # Call the login function
+    createQuestion(page)  # Call the create question function
     while True:
-        login(page)  # Call the login function
-        createQuestion(page)  # Call the create question function
-
         # Wait for user input to continue or break the loop
         user_input = input("run again? Y or N:\n")
         if user_input.lower() != 'y':
             break
+        elif user_input.lower() == 'y':
+            createQuestion(page)
+
 
     browser.close()
+
+
 
 with sync_playwright() as playwright:
     run(playwright)
