05381ac4b1f78f04e8900452b440a8bf314e9a21
stully18 <stullyrl@gmail.com>
2025-03-21 14:28:00 -0400

    csc 220

--- CSC 220 Project/functions.py
+++ CSC 220 Project/functions.py
@@ -1,10 +1,24 @@
+import math
+import threading
+
+global threads
+
+threads = 12
+start = 1
+end = 2000000
+countNumber = 100000
+total_time = 0
+
 def calculatePrimeNumbers(start, end):
     primeNumbers = []
     for i in range(start, end):
+        if i < 2:
+            continue
         prime = True
-        for j in range(2, i):
+        for j in range(2, int(math.sqrt(i)) + 1):
             if i % j == 0:
                 prime = False
+                break
         if prime:
             primeNumbers.append(i)
 
@@ -12,3 +26,21 @@ def countToThisNumber(n):
     square = []
     for i in range(1, n):
         k = i*i
+
+def threadedPrime(end):
+    chunkSize = int(end / threads)
+
+    threadLst = []
+    for i in range(threads):
+        if i == 0:  # First iteration
+            start = 1
+            end = chunkSize
+        else:
+            start = i * chunkSize
+            end = (i + 1) * chunkSize
+        thread = threading.Thread(target=calculatePrimeNumbers, args=(start, end)) 
+        threadLst.append(thread)
+    for i in threadLst:
+        i.start()
+    for i in threadLst:
+        i.join()
--- CSC 220 Project/main.py
+++ CSC 220 Project/main.py
@@ -1,65 +1,13 @@
-import threading
 import timeit
-from functions import calculatePrimeNumbers, countToThisNumber
+from functions import threadedPrime, calculatePrimeNumbers, threads, start, end
 
-global threads
-threads = 12
-start = 1
-end = 10000
-countNumber = 100000
+iterations = 1
 
-def threadedPrime(end):
-    chunkSize = int(end / threads)
+time_threaded = timeit.timeit(lambda: threadedPrime(end), number=iterations)
+print(f"{threads} threads: {round(time_threaded/iterations, 4)} seconds")
 
-    threadLst = []
-    for i in range(threads):
-        if i == 0:  # First iteration
-            start = 1
-            end = chunkSize
-        else:
-            start = i * chunkSize
-            end = (i + 1) * chunkSize
-        thread = threading.Thread(target=calculatePrimeNumbers, args=(start, end)) 
-        threadLst.append(thread)
-    for i in threadLst:
-        i.start()
-    for i in threadLst:
-        i.join()
-
-def threadedCount(countNumber):
-    global threads
-    chunkSize = int(countNumber / threads)
-    threadLst = []
-    for i in range(threads):
-        if i == 0:  # First iteration
-            start = 1
-            end = chunkSize
-        else:
-            start = i * chunkSize
-            end = (i + 1) * chunkSize
-        thread = threading.Thread(target=calculatePrimeNumbers, args=(start, end)) 
-        threadLst.append(thread)
-    for i in threadLst:
-        i.start()
-    for i in threadLst:
-        i.join()
-
-iteration = 10
-total_time = 0
-
-for i in range(iteration):
-    time_threaded = timeit.timeit(lambda: threadedPrime(end), number=1)
-    print(f"Time iteration {i}: {round(time_threaded, 4)} seconds")
-    total_time += time_threaded
-
-average_time = total_time / iteration
-
-print(f"{threads} threads: {round(average_time, 4)} seconds")
-
-print(f"{threads} threads: {round(time_threaded, 4)} seconds")
-
-time_single_thread = timeit.timeit(lambda: calculatePrimeNumbers(start, end), number=1) 
-print(f"Single thread: {round(time_single_thread, 4)} seconds")
+time_single_thread = timeit.timeit(lambda: calculatePrimeNumbers(start, end), number=iterations) 
+print(f"Single thread: {round(time_single_thread/iterations, 4)} seconds")
 
 
 
